7b12c9c1d8fa65f7432c0e5084bfea1277d49249
Lizzie Anthony <lizziewrab@me.com>
2021-11-15 19:24:59 -0600

    commit tree 95acee7d67ecdb45d77b00bcb198e4b23ff99420

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+const body = document.getElementsByTagName('body')[0];
+
+function submitData(firstName, email){
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email,
+        }),
+    };
+    return fetch('http://localhost:3000/users', configObj)
+     .then(response => response.json())
+     .then(object => {
+         document.getElementsByTagName('body')[0].innerHTML = object.id
+     })
+     .catch(error => {
+         console.log(error)
+        // console.log(error.document.body)
+        // console.log(error.message)
+        document.body.innerHTML = error.message
+    })
+        
+     // .catch(error => addNewError(error.document.body))
+
+    //  .catch(function (error){
+    //     alert("Bad things! Ragnarők!");
+    //     console.log(error.message);
+    //  });
+ }
+function addNewError(error){
+    document.getElementsByTagName('body').innerHTML = error;
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName('body')[0].innerHTML = newID;
+}
+submitData("name", "email@email.com")
